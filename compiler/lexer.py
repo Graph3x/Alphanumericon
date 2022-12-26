90568b359b669d13ae797108649305637de26a46
Graph3x <hejzlar.filip@seznam.cz>
2022-12-26 16:13:21 +0100

    updating to alphanumerical

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -7,15 +7,15 @@ class Lexer():
 
     def add_tokens(self):
         # Print
-        self.lexer.add('PRINT', r'print')
+        self.lexer.add('PRINT', r'PRINT')
         # Parenthesis open and closed
-        self.lexer.add('OPEN_PAREN', r'\(')
-        self.lexer.add('CLOSE_PAREN', r'\)')
+        self.lexer.add('OPEN_PAREN', r'sI')
+        self.lexer.add('CLOSE_PAREN', r'Is')
         # Semi Colon
-        self.lexer.add('SEMI_COLON', r'\;')
+        self.lexer.add('SEMI_COLON', r'e')
         # Plus and minus
-        self.lexer.add('SUM', r'\+')
-        self.lexer.add('SUB', r'\-')
+        self.lexer.add('SUM', r'plus')
+        self.lexer.add('SUB', r'minus')
         # Numbers
         self.lexer.add('NUMBER', r'\d+')
         # Ignore spaces
